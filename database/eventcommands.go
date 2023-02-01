0d4ab27411bc8758a641728e1614899e9f9e0073
harvey bal <harvey_bal@hotmail.co.uk>
2023-02-01 15:34:12 +0000

    updated database listen to remove duplications

--- database/eventcommands.go
+++ database/eventcommands.go
@@ -29,6 +29,7 @@ func (m *mongoDBEventsImpl[T]) ListenByEventID(id string) <-chan DataWrapper[mes
 	events := make(chan DataWrapper[message.EventMessage[T]])
 
 	go func() {
+		cache := make(map[string]bool)
 		for {
 			results := make([]DataWrapper[message.EventMessage[T]], 0)
 			curr, err := m.collection.Find(context.TODO(), bson.M{"eventID": id})
@@ -39,15 +40,14 @@ func (m *mongoDBEventsImpl[T]) ListenByEventID(id string) <-chan DataWrapper[mes
 			if err != nil {
 				panic(err)
 			}
-
-			if len(results) == 2 {
-				for _, res := range results {
+			for _, res := range results {
+				if _, ok := cache[res.ID.Hex()]; !ok {
+					cache[res.ID.Hex()] = true
 					events <- res
 				}
-				break
 			}
-			for _, res := range results {
-				events <- res
+			if len(cache) == 2 {
+				break
 			}
 			time.Sleep(100 * time.Millisecond)
 		}
